19419bf67edb1cc6b250c7ff1ba4777178deebcc
kmruddy <kmruddy@gmail.com>
2020-05-27 13:26:09 -0400

    update resource group

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "azurerm_public_ip" "catapp-pip" {
 resource "azurerm_managed_disk" "catdisk" {
   name                 = "${var.prefix}-catdisk"
   location             = var.location
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = azurerm_resource_group.myresourcegroup.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = "100"
