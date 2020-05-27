ae828ce93ee2fdfff9acff6c905eb281d2b9660b
kmruddy <kmruddy@gmail.com>
2020-05-27 12:49:46 -0400

    update machine size

--- variables.tf
+++ variables.tf
@@ -42,7 +42,7 @@ variable "subnet_prefix" {
 
 variable "vm_size" {
   description = "Specifies the size of the virtual machine."
-  default     = "Standard_A0"
+  default     = "Standard_D2"
 }
 
 variable "image_publisher" {
