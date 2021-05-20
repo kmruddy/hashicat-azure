b633ae900b8ad530697f37e74dbb066e43faa037
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-05-20 11:41:38 -0400

    Upgrading Azure Provider Version

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-provider "azurerm" {
-  version = "=1.44.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "1.44.0"
+    }
+  }
+}
 
+provider "azurerm" {
   subscription_id = var.subscription_id
   client_id = var.client_id
   client_secret = var.client_secret
