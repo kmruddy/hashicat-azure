9675517d5b0e615e97d44f4b01a00f5c82aa40c3
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-05-26 17:09:04 -0400

    Add support for updated Terraform versions

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "${var.prefix}-workshop"
+  name     = "${var.prefix}-tfc-demo"
   location = var.location
 }
 
